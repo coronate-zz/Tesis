7324903a9e95b35f17323711718e748422666d98
coroante <acoronadn@gmail.com>
2017-06-10 21:09:18 -0500

    Se agregan reglas script para borrar

--- replace.sh
+++ replace.sh
@@ -1,3 +1,40 @@
 #!/bin/bash
 
-sed 's/> prin.*/ /g' file.txt 
+sed 's/> prin.*/ /g' file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/\[.*/ /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/Resid.*\n     Min.*\n[-+]?([0-9]*\.[0-9]+|[0-9]+).*/ /g' file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/> prin.*/ /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/\[.*/ /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/> summ.*/ /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/\*\*\* Sta.*/ /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/Signif\. .*/ /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+
+sed 's/F-statisti.*/ /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/\n\n/\n/g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/degrees of freedom/ /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/\n\n/\n/g'  file.txt > file2.txt ;
+
+
+sed 's/>/ /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/Residual sta.* on/DF: /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/---/ /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/>/ /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+sed 's/Multiple.*  Adjusted R-squared:/Adjusted R-squared: /g'  file.txt > file2.txt ;
+mv file2.txt file.txt ;
+
+
